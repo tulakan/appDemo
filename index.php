a138f366a8444007a6bae84a57781848b778638b
Kanrawee Karaboot <tulakan17@gmail.com>
2017-01-29 19:32:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,2 @@
-<<<<<<< HEAD
-<?php 
-function GetWeather($state,$country,$langauge,$waetherIcons){
-// auf der Basis von http://www.web-spirit.de/webdesign-tutorial/9/Wetter-auf-eigener-Website-mit-Google-Weather-API
-        $api = simplexml_load_string(utf8_encode(file_get_contents("http://www.google.com/ig/api?weather=".$state."-".$country."&hl=".$langauge)));
-        // $trans = array("Ã¤" => "ä", "Ã„" => "Ä", "Ã¼" => "ü", "Ãœ" => "Ü", "Ã¶" => "ö", "Ã–" => "Ö", "ÃŸ" => "ß");
-        $trans = array("ü" => "ü", "ö" => "ö", "ä" => "ä", "ß" => "ß");
-        $weather = array();
-        $i = 1;
-        foreach($api->weather->forecast_conditions as $GoogleWeather){
-                $waetherIcons = $GoogleIcons = "/ig/images/weather/";
-                $weather[$i]['zustand']                         = strtr(strtolower($GoogleWeather->condition->attributes()->data), $trans);
-                $weather[$i]['tiefsttemperatur']        = $GoogleWeather->low->attributes()->data;
-                $weather[$i]['hoechsttemperatur']       = $GoogleWeather->high->attributes()->data;
-                $weather[$i]['icon']                            = str_replace($GoogleIcons, $waetherIcons, $GoogleWeather->icon->attributes()->data);
-                $i++;
-        }
-        return $weather;
-}
-$result = GetWeather('ca', 'usa', 'en', 'true');
-echo $result
-?>
-=======
 <?php include_once("index.html"); ?>
->>>>>>> 0819d1b13ea6b3d718d2c291fb2ed68d2e249edd
+
