55a077a81a8a32864baab82e91fea0c4ff1b873a
tulakan <tulakan17@gmail.com>
2017-01-30 18:18:22 +0700

    nmnnm

--- bot.php
+++ bot.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * Send debug code to the Javascript console
- */ 
-function console_log( $data ){
-  echo '<script>';
-  echo 'console.log('. json_encode( $data ) .')';
-  echo '</script>';
-}
-
-$myvar = array(1,2,3);
-console_log( $myvar ); // [1,2,3]
-
 $access_token = 'tr8HLJ2dhtefwQfJmfwNRaDmby+sPq+Hig5ut3kN/fehhXey8kttyk/aZCzi1/Xt+9CIYLHaLMUr0ZP58JvdTGHW0Xxqop3vA0CtHJsxkrcvNU20TtFntWrDlo30+QJFGXLW1of5cAfw1IDl0M5UqgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
