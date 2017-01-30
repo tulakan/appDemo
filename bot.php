6568b44bb9dd3f601b650aedfed5bd0e413919ac
tulakan <tulakan17@gmail.com>
2017-01-30 12:58:26 +0700

    masage + 1

--- bot.php
+++ bot.php
@@ -18,8 +18,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
+			{
 				'type' => 'text',
-				'text' => $text
+				'text' => "Hello User"
+			},
+			{
+				'type' => 'text',
+				'text' => "Can I help U ?"
+			}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
