41e2d726afe7d41b2cb0a7c777a239e335d4bffb
tulakan <tulakan17@gmail.com>
2017-01-30 13:14:39 +0700

    f

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-	echo $events;
+	echo "events";
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
