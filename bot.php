ee67a5207ba73788fead98630375b64027730d51
Kanrawee Karaboot <tulakan17@gmail.com>
2017-03-11 01:06:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -108,7 +108,7 @@ if (!is_null($events['events'])) {
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => [$mes3],
+                'messages' => [$mes1,$mes2],
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
